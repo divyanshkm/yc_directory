17210e4a79ffb9e380d8949f7d0cf4a25642fbdb
Divyansh Mahavar <79765685+divyanshkm@users.noreply.github.com>
2024-12-08 00:29:47 +0530

    Search Form Component and shadcn done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { SearchForm } from "../../components/ui/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
   return (
     <>
-      <h1 className="heading">HOME</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          PITCH YOUR STARTUP <br /> WITH US.
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit ideas and get noticed in the world of startups
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
